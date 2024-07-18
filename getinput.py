803e658c2c6e3b8e1e026d6ecd226d72bef4541b
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-18 22:29:02 +0530

    Input

---
+++ getinput.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
