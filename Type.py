2ef34489e830054258b43a7c6ee54df0c14b2f77
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-25 22:57:05 +0530

    use of type

---
+++ Type.py
@@ -0,0 +1,10 @@
+#type_of_variable
+
+name="Parth"
+age=20
+sum=20.1
+
+print(type(name))  #<class 'str'>
+print(type(age)) #<class 'int'>
+print(type(sum))  #<class 'float'>             
+                    
