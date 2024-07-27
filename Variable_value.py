56b42dab1636229465c41fbfe89c257d9754859c
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-27 23:24:21 +0530

    Single_line_if

---
+++ Variable_value.py
@@ -0,0 +1,10 @@
+#<var> = <var1> if < condition > else <var2>
+
+food1 = input("Food : ")
+eat="Yes " if food1 == "cake" else "no"
+print(eat)
+
+sweet=input("Enter jelebi or maggie : ")
+food="yes " if sweet=="jelebbi" else "no"
+food2="no" if sweet=="maggie" else "yes"
+print(food)
