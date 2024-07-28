ac8c9654ab2116bbadb2864c0c70903c59a516b9
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-28 23:10:19 +0530

    All_operators

---
+++ Operators.py
@@ -0,0 +1,48 @@
+
+
+#Arithmetic Operator 
+
+a = 5 
+b = 2
+print("Addition : ",a+b)
+print("Minus : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
+print("Modulus : ",a%b)
+print("a**b : ",a**b)
+
+#Relational Operator
+print("\n")
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a<b)
+print(a>=b)
+print(a>b)
+
+#Assignment operator
+print("\n")
+num=10
+num += 10
+"""
+num -= 2
+num *= 2 
+num /= 2
+num **= 2
+    """
+print(num)
+
+#Logical operator
+print("\n")
+n = 20 
+p = 30
+print("NOT operator : ",not(n < p))  
+
+T = True
+F = False
+print("AND operator : ",T and F)
+
+print("OR operator : ",T or F)
+
+
+
