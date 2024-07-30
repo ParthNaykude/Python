89bb0839a1dcdd9be8033e169b72e9902a1adcfb
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-30 22:11:12 +0530

    string indexing

---
+++ indexing.py
@@ -0,0 +1,22 @@
+#indexing
+str = "Apna_college"
+ch = str[0]
+ch1 = str[1]
+ch2 = str[2]
+
+print(ch + " " +ch1 + " " + ch2)
+
+#Slicing
+
+str1 = "Parth_Naykude"
+S = (str1 [0 : 5])
+print(S)
+l = (str1[5 : len(str1)])
+print(l)
+
+#Negative Slicing 
+
+str2 = "APPLE"
+      #-5 -4 -3 -2 -1
+k = str2[-5 : -1]  
+print(k)    
