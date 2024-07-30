89f280d3a591c3a9df202891426605dfba4451b2
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-30 22:12:29 +0530

    Creating tuple

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#tuple 
+
+tup = (2,6,5,8,9)
+print(tup)
+print(type(tup))
+print(tup[1 : 5])
+
+#tuple_Method 
+tup1 = (2,5,8,9,10)
+
+i = tup1.index(2) #return index of first occerence
+print("\n index of element 2 : ", i )
+
+l=tup.count(2)   # counts total occurences 
+print("\n count of element 2 : " , l)
+
+
