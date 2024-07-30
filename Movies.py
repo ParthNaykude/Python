de89e2c84504ebb4a74bb5fd484c63a0fcc3fdfd
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-30 22:17:30 +0530

    Making_list_input

---
+++ Movies.py
@@ -0,0 +1,14 @@
+# make a list add fav movies in list 
+
+Movies = [] # Empty list 
+
+Mov1 = input("Enter the 1st movie name : ")
+Mov2 = input("Enter the 2nd movie name : ")
+Mov3 = input("Enter the 3rd movie name : ")
+
+Movies.append(Mov1)
+Movies.append(Mov2)
+Movies.append(Mov3)
+
+print("Movies : ",Movies )
+
