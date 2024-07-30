768af8e0ca5282ca7418a1e307d26320a7898498
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-07-30 22:11:37 +0530

    creating List

---
+++ List_in_python.py
@@ -0,0 +1,13 @@
+#List_in_python
+
+Marks = [90,92,87,90,92] # integer list
+print(Marks)
+print(type(Marks)) 
+print(Marks.sort())
+print(len(Marks))
+print(Marks[0])
+
+Students = [ " Parth ", 95.5,20,"Maharashtra"]
+print(Students)
+Students[0] = "Arjun"
+print(Students) # new list 
