d275a016271d7362ef9354a6aeb703565f88bc85
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-08-02 22:50:15 +0530

    add_update_Key_value

---
+++ Dictionary_.py
@@ -0,0 +1,15 @@
+#Dictionary
+
+dict={
+    
+    "name" : "Parth",
+    "CGPA" : 9.6 , 
+    "Age " : 20 ,
+    "Marks" : [90 , 92 , 93],
+}
+
+print(dict["name"])   #printing the value of key name 
+
+dict["name"] = "Parth_Naykude" # update the value of key
+dict["Surname"] = "Naykude"   #Add Key and Value
+print(dict)
