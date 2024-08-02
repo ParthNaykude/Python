b6918412744fd7f7269ed9bd4c10c98dc2bb553f
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-08-02 22:24:46 +0530

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+#Dictionary 
+
+dict = {
+    #Key   :  value 
+    "name" : "Parth",
+    "cgpa" : 9.9,
+    "marks" : [98, 90 ,99],
+    "learning" : "CSE(AI)",
+    "Age" : 20.0,
+
+
+}
+print(dict)
